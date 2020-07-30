0915c5a055befb6a4aa3c2d7f4298011109ffc71
Izzy <avrisaac555@gmail.com>
2020-07-30 06:35:17 +0000

    Basic app structure

--- gestalt.py
+++ gestalt.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'OEYWKSP EHEOEVROEBDLD '
+socketio = SocketIO(app)
+
+@app.route('/')
+def app_view():
+	
+
+if __name__ == '__main__':
+	socketio.run(app)
