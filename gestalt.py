e12d76cb68e346b25d715b1c95d789b92cd5323c
Izzy <avrisaac555@gmail.com>
2020-07-30 13:22:35 -0700

    Defined a message event on socketio

--- gestalt.py
+++ gestalt.py
@@ -1,11 +1,14 @@
 #! /usr/bin/python3
 
+from bleach import Cleaner
+from bleach.linkifier import LinkifyFilter
 from sanic import Sanic, response
-import socketio
 
 STATIC_ASSETS_PATH = "./static"
 LISTEN_HOST = "127.0.0.1"
 LISTEN_PORT = 9000
+
+inputCleaner = Cleaner(filter=[LinkifyFilter])
 
 sio = socketio.AsyncServer(async_mode='sanic')
 app = Sanic(name="gestalt")
@@ -13,8 +16,13 @@ sio.attach(app)
 app.static("/static", STATIC_ASSETS_PATH)
 
 @app.route('/')
-def app_view(request):
+async def app_view(request):
 	return response.file("chat.html")
+
+@sio.event()
+async def message(sid, data):
+	sanitizedText = inputCleaner.clean(data.text)
+	sio.emit("message", {"text": sanitizedText})
 
 if __name__ == "__main__":
 	app.run(host=LISTEN_HOST, port=LISTEN_PORT)
