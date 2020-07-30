1644973a9eafebd76b06191ba6a641015b4bdf01
Izzy <avrisaac555@gmail.com>
2020-07-30 13:10:37 -0700

    Added socketio

--- gestalt.py
+++ gestalt.py
@@ -1,12 +1,15 @@
 #! /usr/bin/python3
 
 from sanic import Sanic, response
+import socketio
 
 STATIC_ASSETS_PATH = "./static"
 LISTEN_HOST = "127.0.0.1"
 LISTEN_PORT = 9000
 
+sio = socketio.AsyncServer(async_mode='sanic')
 app = Sanic(name="gestalt")
+sio.attach(app)
 app.static("/static", STATIC_ASSETS_PATH)
 
 @app.route('/')
