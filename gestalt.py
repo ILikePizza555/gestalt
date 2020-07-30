f6ee9c11f81b09484704d000b664cc08c8b9747f
Izzy <avrisaac555@gmail.com>
2020-07-30 13:07:19 -0700

    static file handling

--- gestalt.py
+++ gestalt.py
@@ -1,10 +1,17 @@
+#! /usr/bin/python3
+
 from sanic import Sanic, response
 
-app = Sanic()
+STATIC_ASSETS_PATH = "./static"
+LISTEN_HOST = "127.0.0.1"
+LISTEN_PORT = 9000
+
+app = Sanic(name="gestalt")
+app.static("/static", STATIC_ASSETS_PATH)
 
 @app.route('/')
-def app_view():
+def app_view(request):
 	return response.file("chat.html")
 
 if __name__ == "__main__":
-	app.run(host="127.0.0.1", port=9000)
+	app.run(host=LISTEN_HOST, port=LISTEN_PORT)
